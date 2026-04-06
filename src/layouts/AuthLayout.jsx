4568ea95b5b6594849f863810cfcabbb27a0e36f
Muhammad <muhammadevvv@gmail.com>
2026-04-06 11:37:24 +0500

    m

--- src/layouts/AuthLayout.jsx
+++ src/layouts/AuthLayout.jsx
@@ -2,7 +2,7 @@ import { Outlet } from "react-router-dom";
 
 export default function AuthLayout() {
   return (
-    <main className="min-h-screen flex items-center justify-center">
+    <main className="flex items-center justify-center">
       <Outlet />
     </main>
   );
