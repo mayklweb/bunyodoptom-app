6bfd2939c09ff8a7aa9c7bbb379164c80286b6c6
Muhammad <muhammadevvv@gmail.com>
2026-05-06 13:52:07 +0500

    m

--- src/widgets/cart/EmptyCart.jsx
+++ src/widgets/cart/EmptyCart.jsx
@@ -4,18 +4,17 @@ import { CartIcon } from "../../assets/icons";
 
 function EmptyCart() {
   return (
-    <div className="flex-1 flex flex-col items-center justify-center text-center gap-3">
-      <div className="w-20 h-20 rounded-2xl bg-gray-100 flex items-center justify-center">
+    <div className="flex-1 flex flex-col items-center justify-center text-center gap-2">
+      <div className="w-20 h-20 mb-4 rounded-2xl bg-white flex items-center justify-center">
         <CartIcon className="w-10 h-10 text-gray-400" />
       </div>
-      <h3 className="text-2xl font-semibold">Savat bo'sh</h3>
-      <p className="text-gray-500 text-base">
-        Hozircha savatda mahsulot yo'q.
-        <br /> Xarid qilishni boshlang!
+      <h3 className="text-lg font-semibold">Voy! Bu yerda hozircha hech narsa yoʻq</h3>
+      <p className="text-gray-500 text-sm">
+      Yoqqan mahsulotlaringizni savatga qoʻshing — ular <br /> bu yerda paydo boʻladi. Xaridlaringiz maroqli boʻlsin!
       </p>
       <Link
         href="/categories"
-        className="text-white px-6 py-3 font-semibold rounded-xl bg-primary"
+        className="text-sm text-white px-6 py-3 font-semibold rounded-xl bg-primary"
       >
         Xaridni boshlash
       </Link>
