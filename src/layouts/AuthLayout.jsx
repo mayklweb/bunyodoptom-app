426dca215c6ec3178536dc973c8e4be0d633b47f
Muhammad <muhammadevvv@gmail.com>
2026-05-12 10:48:10 +0500

    m

--- src/layouts/AuthLayout.jsx
+++ src/layouts/AuthLayout.jsx
@@ -2,7 +2,7 @@ import { Outlet } from "react-router-dom";
 
 export default function AuthLayout() {
   return (
-    <main>
+    <main className="w-full h-full">
       <Outlet />
     </main>
   );
